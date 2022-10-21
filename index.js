f78c3948605978183929635366ac7fc8e5883a98
Maria Fernanda Gomez Ibarra <21031723p@gmail.com>
2022-10-21 10:29:53 -0500

    Primera version del modulo de matemaricas

---
+++ index.js
@@ -0,0 +1,50 @@
+module.exports = {
+   /**
+    * Suma de dos numeros 
+    * @example
+    *  param1  = 1 , patram2 = 5  => resultado 6
+    * @param {*} param1 
+    * @param {*} patram2 
+    * @returns 
+    */
+    suma: function (param1, patram2) {
+        
+        return param1 + patram2;
+    },
+       /**
+        * REsta de dos numeros 
+    * @example
+    *  param1  = 1 , patram2 = 5  => resultado 4
+    * @param {*} param1 
+    * @param {*} patram2 
+    * @returns 
+    */
+    resta: function (param1, patram2) {
+        
+        return param1 - patram2;
+    },
+       /**
+        * multi de dos numeros 
+    * @example
+    *  param1  = 1 , patram2 = 5  => resultado 5
+    * @param {*} param1 
+    * @param {*} patram2 
+    * @returns 
+    */
+    multi: function (param1, patram2) {
+        
+        return param1 * patram2;
+    },
+       /**
+        * division de dos numeros 
+    * @example
+    *  param1  = 5 , patram2 = 2  => resultado 2.5
+    * @param {*} param1 
+    * @param {*} patram2 
+    * @returns 
+    */
+    divi: function (param1, patram2) {
+        
+        return param1 / patram2;
+    },
+}
---
+++ pruebas.js
@@ -0,0 +1,10 @@
+const m = require('.');
+
+
+console.log(m.suma(1,2));
+
+
+console.log(m.resta(1,2));
+console.log(m.multi(1,2));
+console.log(m.divi(1,2));
+console.log(m.suma(1234,232423));
